2037cd72a80e365d937795f4d146bbd1feefb980
cbuelvasc <cbuelvasc@gmail.com>
2020-04-23 18:08:53 -0500

    Firts commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LogginComponent} from './auth/loggin/loggin.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {IncomeEgressComponent} from './income-egress/income-egress.component';
+import {StatisticsComponent} from './income-egress/statistics/statistics.component';
+import {DetailComponent} from './income-egress/detail/detail.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
+import {SidebarComponent} from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IncomeEgressComponent,
+    StatisticsComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +33,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
